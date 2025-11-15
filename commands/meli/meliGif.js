d7ba2d823b9d442f2e469e0f2771c913f852ea5b
Martin M. <drekgx@gmail.com>
2025-11-15 17:42:32 -0300

    Comandos Disponibles: - /meli para buscar gif de gatos

--- commands/meli/meliGif.js
+++ commands/meli/meliGif.js
@@ -1,12 +1,34 @@
 const { SlashCommandBuilder } = require('discord.js');
+const superagent = require('superagent');
+const { tenorApiKey } = require('../../config.json');
+
 
 module.exports = {
 	// nombre del comando /meli-gif
-	data: new SlashCommandBuilder().setName('meli-gif').setDescription('meli buscadora de gif.'),
+	data: new SlashCommandBuilder().setName('meli').setDescription('buscar gifs de meli'),
 	async execute(interaction) {
-		await interaction.reply(
-			'bot meli ha encontrado los siguientes gif.',
-		);
+
+		await interaction.deferReply({});
+
+		const search_term = 'cat';
+		const apikey = tenorApiKey;
+		const clientkey = 'meli-discord-bot';
+		const lmt = 8;
+
+		const choice = Math.floor(Math.random() * lmt);
+		const search_url = 'https://tenor.googleapis.com/v2/search?q=' + search_term + '&key=' + apikey + '&client_key=' + clientkey + '&limit=' + lmt;
+
+
+		try {
+			const output = await superagent.get(search_url);
+
+			const gifUrl = output.body.results[choice].itemurl;
+
+			await interaction.editReply({ content: gifUrl });
+
+		} catch (e) {
+			console.error(e);
+			return await interaction.editReply({ content: `No encontré ningún gif con el nombre \`${search_term}\`!` });
+		}
 	},
 };
-
